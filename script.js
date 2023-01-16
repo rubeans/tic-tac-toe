8df74c5bddc21542645dbdd49b3e3e153438cd61
rubeans <rubensglf@gmail.com>
2023-01-16 16:45:24 -0300

    fix body height and colors design

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const optionBox = document.querySelectorAll('.option-box')
 optionBox.forEach(opt => opt.addEventListener('click', () => {
+    opt.style.color = '#7aefd8'
     opt.textContent = 'X'
 }))
