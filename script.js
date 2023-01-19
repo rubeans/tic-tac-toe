3de04daea1df9690994e93ffe2ce1248423c1334
rubeans <rubensglf@gmail.com>
2023-01-18 22:30:45 -0300

    add favicon

--- script.js
+++
@@ -1,5 +0,0 @@
-const optionBox = document.querySelectorAll('.option-box')
-optionBox.forEach(opt => opt.addEventListener('click', () => {
-    opt.style.color = '#7aefd8'
-    opt.textContent = 'X'
-}))
