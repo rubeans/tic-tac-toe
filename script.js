4be0c1b0eb273a884bb07bce4c1396fcace9aa90
rubeans <rubensglf@gmail.com>
2023-01-24 09:20:31 -0300

    take off custom colors and let everything the color

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // CREATE PLAYERS
-const Players = (maker, color) => ({ maker, color })
+const Players = (maker) => ({ maker })
 
 // GAME FLOW
 const Game = (() => {
@@ -7,8 +7,8 @@ const Game = (() => {
     const boxes = document.querySelectorAll('.option-box')
     const statusTxt = document.querySelector('.statusTxt')
     const restartBtn = document.querySelector('.restart-btn')
-    const xPlayer = Players("X", "#7aefd8")
-    const oPlayer = Players("O", "#fd9b9b")
+    const xPlayer = Players("X")
+    const oPlayer = Players("O")
     const winConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -27,7 +27,6 @@ const Game = (() => {
     function startGame() {
         boxes.forEach(box => box.addEventListener('click', boxClicked))
         restartBtn.addEventListener('click', restartGame)
-        statusTxt.style.color = xPlayer.color
         statusTxt.textContent = `${currentPlayer}'s turn`
         running = true
     }
@@ -47,10 +46,6 @@ const Game = (() => {
     }
 
     function changePlayer() {
-        boxes.forEach(box => {
-            box.textContent === xPlayer.maker ? box.style.color = xPlayer.color : box.style.color = oPlayer.color
-        })
-        currentPlayer === xPlayer.maker ? statusTxt.style.color = oPlayer.color : statusTxt.style.color = xPlayer.color
         currentPlayer = (currentPlayer === xPlayer.maker) ? oPlayer.maker : xPlayer.maker
         statusTxt.textContent = `${currentPlayer}'s turn`
     }
